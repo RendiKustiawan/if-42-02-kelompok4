f7ca09c061eabd61371c2e37836082bd0acea08c
Muhammad Rayhan Hakim <mrayhanhakim2@gmail.com>
2020-04-16 13:02:22 +0700

    Template dashboard at 100% :)

--- application/views/DashboardView.php
+++ application/views/DashboardView.php
@@ -1,4 +1,3 @@
 <div class="dashboard-container col">
   <!-- <a href="<?= site_url('Landing/logout') ?>">logout</a> -->
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, maiores illo. Alias ut nemo laudantium saepe ducimus enim delectus accusantium, labore consequuntur tenetur autem eius voluptatem, obcaecati ab vel rem!
 </div>
--- application/views/template/SideNav.php
+++ application/views/template/SideNav.php
@@ -1,22 +1,22 @@
-<aside class="side-nav-wrapper d-md-flex flex-column col-lg-2 col-md-3 d-none">
+<aside class="side-nav-wrapper col-lg-2 col-md-3" id="sideNav">
   <div class="side-nav-container">
     <a href="<?= site_url('Landing/dashboard') ?>">
       <i class="fas fa-columns"></i>
       Dashboard
     </a>
-    <a href="<?= site_url('AkunController') ?>">
+    <a href="<?= site_url('AkunController') ?>" class="<?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
       <i class="fas fa-user-circle"></i>
       Akun
     </a>
-    <a href="<?= site_url('AdminController') ?>">
+    <a href="<?= site_url('AdminController') ?>" class="<?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
       <i class="fas fa-user-cog"></i>
       Admin
     </a>
-    <a href="<?= site_url('DokterController') ?>">
+    <a href="<?= site_url('DokterController') ?>" class="<?php if ($this->session->userdata('hak_akses') == 3) {echo "hide-nav";} ?>">
       <i class="fas fa-user-md"></i>
       Dokter
     </a>
-    <a href="<?= site_url('PasienController') ?>">
+    <a href="<?= site_url('PasienController') ?>" class="<?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
       <i class="fas fa-hospital-user"></i>
       Pasien
     </a>
