d69197a9d5b10b2793dab0eb6126f3619fe5f91d
Muhammad Rayhan Hakim <mrayhanhakim2@gmail.com>
2020-04-16 15:27:05 +0700

    Add div class="col" in Body.php view

--- application/views/Body.php
+++ application/views/Body.php
@@ -8,11 +8,13 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="<?= base_url('assets/css/custom.css') ?>">
+  <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
   <title><?= $title ?></title>
 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css"></script>
 </head>
 
 <body>
@@ -22,8 +24,10 @@
   ?>
     <div class="dashboard-wrapper container-fluid">
       <div class="row d-flex align-items-stretch">
-      <?php $this->load->view('template/SideNav');
-      $this->load->view($main_view); ?>
+        <?php $this->load->view('template/SideNav'); ?>
+        <div class="col">
+          <?php $this->load->view($main_view); ?>
+        </div>
       </div>
     </div>
   <?php
