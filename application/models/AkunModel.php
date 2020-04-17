10a8c2d1a9aa6fe0e4ec55e86dcc0d4669bce024
Muhammad Rayhan Hakim <mrayhanhakim2@gmail.com>
2020-04-18 00:24:29 +0700

    Join Akun with 3 child table

--- application/models/AkunModel.php
+++ application/models/AkunModel.php
@@ -5,7 +5,11 @@ class AkunModel extends CI_Model
 {
     public function GetAkun()
     {
-        $query = $this->db->query('SELECT * FROM akun');
+        $query = $this->db->query('SELECT `akun`.`username`, COALESCE(`admin`.`nama`, `dokter`.`nama`, `pasien_user`.`nama`) `nama`, `akun`.`hak_akses`
+        FROM `akun` 
+            LEFT JOIN `admin` ON `admin`.`username` = `akun`.`username` 
+            LEFT JOIN `dokter` ON `dokter`.`username` = `akun`.`username` 
+            LEFT JOIN `pasien_user` ON `pasien_user`.`username` = `akun`.`username`;');
 
         return $query->result();
     }
