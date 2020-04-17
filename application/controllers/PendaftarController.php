2c6e21b3c64821b804fef4cd8d90fddb3f675cd1
Rendi Kustiawan <rendikstwn27992gmail.com>
2020-04-17 20:33:45 +0700

    update pendaftaran fix

--- application/controllers/PendaftarController.php
+++ application/controllers/PendaftarController.php
@@ -23,4 +23,9 @@ class PendaftarController extends CI_Controller
 
         echo json_encode($data);
     }
+
+    public function delete_pendaftar($id_tabel_pendaftar) {
+        $this->PendaftarModel->deletePendaftar($id_tabel_pendaftar);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data '.$id_tabel_pendaftar.' Berhasil Dihapus</div>');
+    }
 }
