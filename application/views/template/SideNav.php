7ccf95a4b7c566d9c289f202a35d2e11dffa50b8
Muhammad Rayhan Hakim <mrayhanhakim2@gmail.com>
2020-04-18 00:20:26 +0700

    Add active link & change link hover design

--- application/views/template/SideNav.php
+++ application/views/template/SideNav.php
@@ -1,30 +1,30 @@
-<aside class="side-nav-wrapper col-lg-2 col-md-3" id="sideNav">
+<aside class="side-nav-wrapper col-lg-2 col-md-3 col-sm-12" id="sideNav">
   <div class="side-nav-container">
-    <a href="<?= site_url('Landing/dashboard') ?>">
+    <a href="<?= site_url('Landing/dashboard') ?>" class="<?php if ($title == "Dashboard") { echo "active"; } ?>">
       <i class="fas fa-columns"></i>
       Dashboard
     </a>
-    <a href="<?= site_url('AkunController') ?>" class="<?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
+    <a href="<?= site_url('AkunController') ?>" class="<?php if ($title == "Data Akun") { echo "active"; } ?> <?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
       <i class="fas fa-user-circle"></i>
       Akun
     </a>
-    <a href="<?= site_url('AdminController') ?>" class="<?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
+    <a href="<?= site_url('AdminController') ?>" class="<?php if ($title == "Data Admin") { echo "active"; } ?> <?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
       <i class="fas fa-user-cog"></i>
       Admin
     </a>
-    <a href="<?= site_url('DokterController') ?>" class="<?php if ($this->session->userdata('hak_akses') == 3) {echo "hide-nav";} ?>">
+    <a href="<?= site_url('DokterController') ?>" class="<?php if ($title == "Data Dokter") { echo "active"; } ?> <?php if ($this->session->userdata('hak_akses') == 3) {echo "hide-nav";} ?>">
       <i class="fas fa-user-md"></i>
       Dokter
     </a>
-    <a href="<?= site_url('PasienController') ?>" class="<?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
+    <a href="<?= site_url('PasienController') ?>" class="<?php if ($title == "Data Pasien") { echo "active"; } ?> <?php if ($this->session->userdata('hak_akses') != 1) {echo "hide-nav";} ?>">
       <i class="fas fa-hospital-user"></i>
       Pasien
     </a>
-    <a href="<?= site_url('PendaftarController') ?>">
+    <a href="<?= site_url('PendaftarController') ?>" class="<?php if ($title == "Data Pendaftaran") { echo "active"; } ?>">
       <i class="fas fa-notes-medical"></i>
       Pendaftaran
     </a>
-    <a href="<?= site_url('JadwalController') ?>">
+    <a href="<?= site_url('JadwalController') ?>" class="<?php if ($title == "Data Jadwal Pendaftar") { echo "active"; } ?>">
       <i class="fas fa-calendar"></i>
       Jadwal Imunisasi
     </a>
--- application/views/template/TopNav.php
+++ application/views/template/TopNav.php
@@ -28,22 +28,52 @@
 
 <script type="text/javascript">
   $(document).ready(function() {
+    if ($(window).width() < 769) {
+      if (!($("#sideNav").hasClass("d-none"))) {
+        $("#sideNav").toggleClass("d-none");
+        $(".dashboard-container").toggleClass("col-lg-10");
+        $(".dashboard-container").toggleClass("col-md-9");
+        $(".dashboard-container").toggleClass("col-sm-12");
+        $(".dashboard-container").toggleClass("col-12");
+      }
+    } else {
+      if (($("#sideNav").hasClass("d-none"))) {
+        $("#sideNav").toggleClass("d-none");
+        $(".dashboard-container").toggleClass("col-lg-10");
+        $(".dashboard-container").toggleClass("col-md-9");
+        $(".dashboard-container").toggleClass("col-sm-12");
+        $(".dashboard-container").toggleClass("col-12");
+      }
+    }
+
     $("#menuUser").click(function() {
       $("#floatingMenu").toggleClass("d-none");
     })
 
     $(".navbar-toggler").click(function() {
       $("#sideNav").toggleClass("d-none");
+      $(".dashboard-container").toggleClass("col-lg-10");
+      $(".dashboard-container").toggleClass("col-md-9");
+      $(".dashboard-container").toggleClass("col-sm-12");
+      $(".dashboard-container").toggleClass("col-12");
     })
 
     $(window).resize(function() {
       if ($(window).width() < 769) {
         if (!($("#sideNav").hasClass("d-none"))) {
           $("#sideNav").toggleClass("d-none");
+          $(".dashboard-container").toggleClass("col-lg-10");
+          $(".dashboard-container").toggleClass("col-md-9");
+          $(".dashboard-container").toggleClass("col-sm-12");
+          $(".dashboard-container").toggleClass("col-12");
         }
       } else {
         if (($("#sideNav").hasClass("d-none"))) {
           $("#sideNav").toggleClass("d-none");
+          $(".dashboard-container").toggleClass("col-lg-10");
+          $(".dashboard-container").toggleClass("col-md-9");
+          $(".dashboard-container").toggleClass("col-sm-12");
+          $(".dashboard-container").toggleClass("col-12");
         }
       }
     })
