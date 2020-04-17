b346ab3a27024cb583ecbbe8359450de53dbeadc
Rendi Kustiawan <rendikstwn27992gmail.com>
2020-04-17 15:43:54 +0700

    update pendaftar controller

--- application/controllers/PendaftarController.php
+++ application/controllers/PendaftarController.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class PasienController extends CI_Controller
+class PendaftarController extends CI_Controller
 {
     public function __construct()
     {
